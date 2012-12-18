30741777591f257a0d8af5b8a74f9eb2c1c2afb9
Nick Saika <nicksaika@gmail.com>
2012-12-18 13:14:37 -0500

    Added the daemon/mainloop package, and updated the README.

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
 goutils
 =======
 
-Some utility libraries for Go that aren't special enough to warrant their own repository
+Some utility libraries for Go that aren't special enough to warrant their own repository.
+
+`goutils/daemon/mainloop`
+-------------------------
+
+A really simple mainloop that listens for SIGTERM, SIGKILL and SIGHUP signals
+from the operating system.
+
+To use this simple main-loop, after `go get`ting the package, just put
+`mainloop.Start()` at the end of your `main()` function.
+
+```go
+import "github.com/nesv/goutils/daemon/mainloop"
+
+func main() {
+	...
+	mainloop.Start()
+	return
+}
+```
---
+++ daemon/mainloop/mainloop.go
@@ -0,0 +1,78 @@
+/*
+Creates a mainloop, that handles signals received from the operating system.
+*/
+package mainloop
+
+import (
+	"errors"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+var (
+	SignalAlreadyBoundError        = errors.New("Signal is already bound to a function")
+	CannotUnbindUnboundSignalError = errors.New("Signal is not bound to a function")
+)
+
+// This struct represents the mainloop for catching operating system signals
+// in your application.
+type Mainloop struct {
+	sigchan  chan os.Signal
+	Bindings map[syscall.Signal]func()
+}
+
+// Initializes and returns a pointer to a new Mainloop.
+func New() *Mainloop {
+	m := Mainloop{sigchan: make(chan os.Signal),
+		Bindings: make(map[syscall.Signal]func())}
+	return &m
+}
+
+// Bind an operating system signal to a handler function, prior to calling
+// Mainloop.Start().
+//
+// You cannot bind multiple functions to the same signal, and any attempt to
+// do so will raise an error.
+func (m *Mainloop) Bind(sig syscall.Signal, f func()) (err error) {
+	for s, _ := range m.Bindings {
+		if sig == s {
+			err = SignalAlreadyBoundError
+			return
+		}
+	}
+	m.Bindings[sig] = f
+	return nil
+}
+
+// Unbind the currently-bound function from the speicifed operating system
+// signal.
+//
+// If the signal is not bound to a handler function, then this method is
+// effectively a no-op.
+func (m *Mainloop) Unbind(sig syscall.Signal) {
+	delete(m.Bindings, sig)
+	return
+}
+
+// Start the mainloop.
+//
+// This method will block its current thread. The best spot for calling this
+// method is right near the bottom of your application's main() function.
+func (m *Mainloop) Start() {
+	sigs := make([]syscall.Signal, len(m.Bindings))
+	for s, _ := range m.Bindings {
+		sigs = append(sigs, s)
+	}
+	signal.Notify(m.sigchan, sigs...)
+	for {
+		var sig = <-m.sigchan
+		for s, handler := range m.Bindings {
+			if s == sig {
+				handler()
+				break
+			}
+		}
+	}
+	return
+}
